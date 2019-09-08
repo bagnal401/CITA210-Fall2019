ee0aa8066f45ed0b247a849ffe8988b86387ad8e
bagnal401 <bagnal401@morrisville.edu>
2019-09-08 18:43:58 -0400

    Add Assignment 1 to repo, including Assignment-1 project, Assignment1bagnal401.pdf and Screenshot1bagnal401.pdf

---
+++ projects/bagnal401/Assignment-1/Assignment-1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Assignment_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.translationLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sinisterButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.dexterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // translationLabel
+            // 
+            this.translationLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.translationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translationLabel.Location = new System.Drawing.Point(120, 101);
+            this.translationLabel.Name = "translationLabel";
+            this.translationLabel.Size = new System.Drawing.Size(242, 44);
+            this.translationLabel.TabIndex = 0;
+            this.translationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(364, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select a Latin word and I will translate it to English:";
+            // 
+            // sinisterButton
+            // 
+            this.sinisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sinisterButton.Location = new System.Drawing.Point(67, 173);
+            this.sinisterButton.Name = "sinisterButton";
+            this.sinisterButton.Size = new System.Drawing.Size(75, 40);
+            this.sinisterButton.TabIndex = 2;
+            this.sinisterButton.Text = "Sinister";
+            this.sinisterButton.UseVisualStyleBackColor = true;
+            this.sinisterButton.Click += new System.EventHandler(this.SinisterButton_Click);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.Location = new System.Drawing.Point(215, 173);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(75, 40);
+            this.mediumButton.TabIndex = 3;
+            this.mediumButton.Text = "Medium";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            this.mediumButton.Click += new System.EventHandler(this.MediumButton_Click);
+            // 
+            // dexterButton
+            // 
+            this.dexterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dexterButton.Location = new System.Drawing.Point(352, 173);
+            this.dexterButton.Name = "dexterButton";
+            this.dexterButton.Size = new System.Drawing.Size(75, 40);
+            this.dexterButton.TabIndex = 4;
+            this.dexterButton.Text = "Dexter";
+            this.dexterButton.UseVisualStyleBackColor = true;
+            this.dexterButton.Click += new System.EventHandler(this.DexterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 251);
+            this.Controls.Add(this.dexterButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.sinisterButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.translationLabel);
+            this.Name = "Form1";
+            this.Text = "Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label translationLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sinisterButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button dexterButton;
+    }
+}
+
---
+++ projects/bagnal401/Assignment-1/Assignment-1/Form1.cs
@@ -0,0 +1,44 @@
+﻿/* Michelle M. Bagnall
+ * CITA 210: Visual Programming & Development Tools
+ * Assignment 1: Creating a Latin-to-English translator app for three words: sinister, medium, and dexter.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1
+{
+    public partial class Form1 : Form
+    {
+        // Form1 calls the InitializeComponent method to start the app.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // SinisterButton_Click will translate the Latin word "Sinister" to the English word "Left" when the button is clicked.
+        private void SinisterButton_Click(object sender, EventArgs e)
+        {
+            translationLabel.Text = "Left"; // Displays "Left" in the translationLabel control.
+        }
+
+        // MediumButton_Click will translate the Latin word "Medium" to the English word "Center" when the button is clicked.
+        private void MediumButton_Click(object sender, EventArgs e)
+        {
+            translationLabel.Text = "Center"; // Displays "Center" in the translationLabel control.
+        }
+
+        // DexterButton_Click will translate the Latin word "Dexter" to the English word "Right" when the button is clicked.
+        private void DexterButton_Click(object sender, EventArgs e)
+        {
+            translationLabel.Text = "Right"; // Displays "Right" in the translationLabel control.
+        }
+    }
+}
